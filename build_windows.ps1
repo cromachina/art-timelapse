649396405f40960013b7bac5fdb993f4d550b2c0
cromachina <82557197+cromachina@users.noreply.github.com>
2026-06-22 11:57:11 -0400

    Update windows build script

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,8 +1,9 @@
 winget install Python.Python.3.14
 rm build -r -Force
 rm dist -r -Force
-#python -m venv venv
-#. venv/Scripts/activate.ps1
+rm venv -r -Force
+python -m venv venv
+. venv/Scripts/activate.ps1
 pip install -e .[dev]
 pyinstaller art_timelapse.spec
 $VERSION = python -c "from importlib.metadata import version; print(version('art-timelapse'), end='')"
