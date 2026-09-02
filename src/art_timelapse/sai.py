d51aaec6980d5d637a320bbc9b9ae8fffb0e275f
cromachina <82557197+cromachina@users.noreply.github.com>
2026-09-02 17:05:02 -0400

    Add support for SAI Ver.2 (64bit) Alpha.2026.09.02 (supersedes the previous alpha version)

--- src/art_timelapse/sai.py
+++ src/art_timelapse/sai.py
@@ -299,10 +299,10 @@ class SAIv2_API_2026_07_02b(SAIv2_API_Base):
     session_offset = 0x32eb20
 
 @register_sai_api
-class SAIv2_API_2026_08_24_alpha(SAIv2_API_Base):
-    version_name = 'SAI Ver.2 (64bit) Alpha.2026.08.24'
-    exe_hash = 'd6fa0ad949fd65203c7aadc421b3acbf'
-    session_offset = 0x474380
+class SAIv2_API_2026_09_02_alpha(SAIv2_API_Base):
+    version_name = 'SAI Ver.2 (64bit) Alpha.2026.09.02'
+    exe_hash = '9df870ae862425b0e910cc82555faa1e'
+    session_offset = 0x476280
 
     class SAICanvas(SAICanvasBase):
         pass
